318bf4fcc31af8f7e08bfb410f737f9225b20b79
MoBoudni <168630329+MoBoudni@users.noreply.github.com>
2025-05-31 23:10:19 +0200

    Update module-info.java

--- src/module-info.java
+++ src/module-info.java
@@ -1,9 +1,7 @@
 /**
- * 
- */
-/**
- * 
+ * Das Modul LernFit_Kursanmeldung_Variable_Verzweigungen enthält Klassen für die Kursanmeldungsanwendung.
+ * Es benötigt das java.desktop-Modul für die GUI-Komponenten.
  */
 module LernFit_Kursanmeldung_Variable_Verzweigungen {
-	requires java.desktop;
+    requires java.desktop;
 }
